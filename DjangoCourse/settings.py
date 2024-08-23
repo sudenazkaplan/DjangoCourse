d605a67a818f67aaac8dfbc7f0d986de7ed6fe61
Sudenaz Kaplan <sudenazkaplan39@gmail.com>
2024-08-23 15:56:27 +0300

    feat: enable continues development

--- DjangoCourse/settings.py
+++ DjangoCourse/settings.py
@@ -62,7 +62,8 @@ ROOT_URLCONF = 'DjangoCourse.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates']
+        ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- core/views.py
+++ core/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(requests):
+    return render(request, 'index.html')
