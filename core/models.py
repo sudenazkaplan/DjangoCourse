a42ad3cc7e3e49b3ebdab8b9ba42bf3c3487997c
Sudenaz Kaplan <sudenazkaplan39@gmail.com>
2024-08-27 22:40:32 +0300

    feat: create general settings model to admin

--- core/models.py
+++ core/models.py
@@ -2,27 +2,45 @@ from django.db import models
 
 # Create your models here.
 
-class GeneralSettings(models.Model):
+class GeneralSetting(models.Model):
     name = models.CharField(
         default='',
         max_length=254,
         blank=True,
+        verbose_name='Name',
+        help_text='this is variable of the settings.',
     )
     description = models.CharField(
         default='',
         max_length=254,
         blank=True,
+        verbose_name='Description',
+        help_text='',
     )
     parameter = models.CharField(
         default='',
         max_length=254,
         blank=True,
+        verbose_name='Parameter',
+        help_text='',
     )
     updated_date = models.DateTimeField(
         blank=True,
         auto_now=True,
+        verbose_name='Updated_Date',
+        help_text='',
     )
     created_date = models.DateTimeField(
         blank=True,
         auto_now_add=True,
+        verbose_name='Created_Date',
+        help_text='',
     )
+
+    def __str__(self):
+        return f'GeneralSetting: {self.name}'
+
+    class Meta:
+        verbose_name = 'General Setting'
+        verbose_name_plural = 'General Settings'
+        ordering = ('name',)
