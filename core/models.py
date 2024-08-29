a399c45e230c02c3273ac53fd2a529b4c677e206
Sudenaz Kaplan <sudenazkaplan39@gmail.com>
2024-08-29 11:05:17 +0300

    feat: create image setting model

--- core/models.py
+++ core/models.py
@@ -44,3 +44,46 @@ class GeneralSetting(models.Model):
         verbose_name = 'General Setting'
         verbose_name_plural = 'General Settings'
         ordering = ('name',)
+
+
+class ImageSetting(models.Model):
+    name = models.CharField(
+        default='',
+        max_length=254,
+        blank=True,
+        verbose_name='Name',
+        help_text='this is variable of the settings.',
+    )
+    description = models.CharField(
+        default='',
+        max_length=254,
+        blank=True,
+        verbose_name='Description',
+        help_text='',
+    )
+    file = models.FileField(
+        default='',
+        verbose_name='Image',
+        blank=True,
+        help_text='',
+        upload_to='images/',
+    )
+    updated_date = models.DateTimeField(
+        blank=True,
+        auto_now=True,
+        verbose_name='Updated_Date',
+        help_text='',
+    )
+    created_date = models.DateTimeField(
+        blank=True,
+        auto_now_add=True,
+        verbose_name='Created_Date',
+        help_text='',
+    )
+    def __str__(self):
+        return f'ImageSetting: {self.name}'
+
+    class Meta:
+        verbose_name = 'Image Setting'
+        verbose_name_plural = 'Image Settings'
+        ordering = ('name',)
