ec04c1d1f67418182275d9fb90d82144eebfcfeb
Sudenaz Kaplan <sudenazkaplan39@gmail.com>
2024-09-03 18:25:49 +0300

    feat: create message model

--- contact/views.py
+++ contact/views.py
@@ -1,13 +1,34 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from contact.models import Message
 
 
 # Create your views here.
 
 def contact_form(request):
+    if request.POST:
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+        success = True,
+        message = 'İletişim formu başarıyla gönderildi!'
+
+    else:
+        success = False
+        message = 'Önerilen yöntem geçerli değil.'
+
+    Message.objects.create(
+        name=name,
+        email=email,
+        subject=subject,
+        message=message,
+    )
+
     context = {
         'success': True,
-        'message': 'Contact form sent successfully.'
+        'message': 'İletişim formu başarıyla gönderildi!'
     }
     return JsonResponse(context)
 
